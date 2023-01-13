77f0cc4b23425b25459914f9bc31f7ad7836da2a
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-01-13 20:36:13 -0300

    function user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,47 +3,14 @@ const User = require('../models/User');
 
 router.post("/usuario/add", async function (req, res) {
     try {
-        const {
-            nome,
-            foto,
-            cpf,
-            email,
-            senha,
-            telefone,
-            data_nasc
-        } = req.body;
-
-        if (!nome) {
-            res.status(422).json({ message: "Nome obrigatório!" });
-            return;
-        }
-
-        if (!email) {
-            res.status(422).json({ message: "E-mail obrigatório!" });
-            return;
-        } else {
-            let users = await User.find({ email: email });
-            if (users.length != 0) {
-                res.status(422).json({ message: "Usuário já cadastrado!" });
-                return;
-            }
-        }
-
-        const user = {
-            nome,
-            foto,
-            cpf,
-            email,
-            senha,
-            telefone,
-            data_nasc
-        };
-
+        //Receber e montar o usuário
+        const user = montUser(req);
+        //Validar os dados;
+        await validUser(User, user);
         await User.create(user);
         res.status(200).json({ message: "Cadastrado!" });
-
     } catch (error) {
-        res.status(500).json({ error: "Erro ao cadastrar!" });
+        res.status(500).json({ error: error.message });
     }
 });
 
@@ -66,5 +33,62 @@ router.get("/usuario/:id", async function (req, res) {
         res.status(500).json({ error: "Erro ao cadastrar!" });
     }
 });
+
+function montUser(req) {
+    const {
+        nome,
+        foto,
+        cpf,
+        email,
+        senha,
+        telefone,
+        data_nasc
+    } = req.body;
+
+    const user = {
+        nome,
+        foto,
+        cpf,
+        email,
+        senha,
+        telefone,
+        data_nasc
+    };
+
+    return user;
+}
+
+async function validUser(User, dataUser) {
+    let error = 0;
+    if (!dataUser.email) {
+        error++;
+        //res.status(422).json({ message: "E-mail obrigatório!" });
+        //return
+    } else {
+        let users = await User.find({ email: dataUser.email });
+        if (users.length != 0) {
+            error++;
+            //res.status(422).json({ message: "Usuário já cadastrado!" });
+            //return;
+            //throw new Error("Usuário já cadastrado!");
+        }
+    }
+
+    if (!dataUser.nome) {
+        error++;
+        //res.status(422).json({ message: "Nome obrigatório!" });
+        //return;
+    }
+
+    if (!dataUser.senha) {
+        error++;
+        //res.status(422).json({ message: "Senha obrigatório!" });
+        //return;
+    }
+
+    if (error > 0) {
+        throw new Error('Error ao cadastrar ou usuário já cadastrado!');
+    }
+}
 
 module.exports = router;
